86c7b839bbbd8971f6bb18ed2e5d7d7e232d47a1
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-11-24 12:15:17 -0700

    adds UseCast.java to package chapter02

---
+++ src/main/java/com/brandyn/chapter02/UseCast.java
@@ -0,0 +1,15 @@
+package com.brandyn.chapter02;
+
+/**
+ * Using a cast.
+ */
+public class UseCast {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + " / 3: " + i / 3);
+            System.out.println(i + " / 3 with fractions: " +
+                    (double) i / 3);
+            System.out.println();
+        }
+    }
+}
