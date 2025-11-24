2746b1ae1e8c0b3495f90c463b681f82e3e47a04
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-11-24 12:13:10 -0700

    adds PromDemo.java to package chapter02

---
+++ src/main/java/com/brandyn/chapter02/PromDemo.java
@@ -0,0 +1,19 @@
+package com.brandyn.chapter02;
+
+/**
+ * A promotion surprise!
+ */
+public class PromDemo {
+    public static void main(String[] args) {
+        byte b;
+        int i;
+
+        b = 10;
+        i = b * b; // OK, no cast needed
+
+        b = (byte) (b * b); // cast needed!
+
+        System.out.println("i = " + i);
+        System.out.println("b = " + b);
+    }
+}
