5ad77f212a16e9ed52c03fb4097b07bb1db65b7b
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-11-24 10:14:25 -0700

    adds BoolDemo.java to package chapter02

---
+++ src/main/java/com/brandyn/chapter02/BoolDemo.java
@@ -0,0 +1,29 @@
+package com.brandyn.chapter02;
+
+/**
+ * Demonstrate boolean values.
+ */
+public class BoolDemo {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("b = " + b);
+
+        b = true;
+        System.out.println("b = " + b);
+
+        // a boolean value can control the if statement
+        if (b) {
+            System.out.println("This is executed.");
+        }
+
+        b = false;
+        if (b) {
+            System.out.println("This is not executed.");
+        }
+
+        // outcome of a relational operator is a boolean value
+        System.out.println("10 > 9 is " + (10 > 9));
+    }
+}
