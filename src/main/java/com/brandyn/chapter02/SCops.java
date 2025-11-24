004cdcecf554c965df4ec6dbfdb3616c8b0c6be7
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-11-24 11:44:10 -0700

    adds SCops.java to package chapter02

---
+++ src/main/java/com/brandyn/chapter02/SCops.java
@@ -0,0 +1,25 @@
+package com.brandyn.chapter02;
+
+/**
+ * Demonstrate the short-circuit operators.
+ */
+public class SCops {
+    public static void main(String[] args) {
+        int n, d, q;
+
+        n = 10;
+        d = 2;
+        if (d != 0 && (n % d) == 0) {
+            System.out.println(d + " is a factor of " + n);
+        }
+
+        d = 0;
+        if (d != 0 && (n % d) == 0) {
+            System.out.println(d + " is a factor of " + n);
+        }
+
+        if (d != 0 & (n % d == 0)) {
+            System.out.println(d + " is a factor of " + n); // divide-by-zero error
+        }
+    }
+}
